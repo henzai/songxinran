8dad9788d24c02357afb3d415dc2ccc636560406
henzai <ry0chord@gmail.com>
2021-05-26 01:08:39 +0900

    fix addr

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod model;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 8080).into();
+    let addr = ([0, 0, 0, 0], 8080).into();
 
     let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(route)) });
 
